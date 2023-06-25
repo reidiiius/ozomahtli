e60b9dc0fba6ee31b6db436e041e66fa76d4e975
Reid Netterville III <reidiiius@gmail.com>
2023-06-25 14:17:58 -0500

    1687720678

--- test/sondaicus.js
+++ test/sondaicus.js
@@ -19,6 +19,15 @@ const Elapida = {
 };
 
 
+Elapida.tailgate = function(moniker) {
+  console.log("identity: %s", moniker);
+  console.count('test-completed');
+  console.timeLog('timeline');
+
+  return;
+};
+
+
 Elapida.diagnose = function(moniker, anomaly) {
   console.count('caught-exceptions');
   console.log("where: %s", moniker);
@@ -50,11 +59,11 @@ Elapida.examine_returned = function(genus, moniker, ...args) {
 
     console.assert(bool, "%s returns %s", moniker, genus);
 
-   Elapida.scorecard(bool);
-
-    console.count('test-completed');
+    Elapida.scorecard(bool);
   } catch (anomaly) {
     Elapida.diagnose(moniker, anomaly);
+  } finally {
+    Elapida.tailgate(moniker);
   }
 
   return;
@@ -70,11 +79,13 @@ Elapida.examine_datatype = function(genus, moniker) {
     console.assert(bool, "%s is %s", moniker, genus);
 
     Elapida.scorecard(bool);
-
-    console.count('test-completed');
   } catch (anomaly) {
     Elapida.diagnose(moniker, anomaly);
+  } finally {
+    Elapida.tailgate(moniker);
   }
+
+  return;
 };
 
 
@@ -87,136 +98,83 @@ Elapida.examine_typified = function(genus, moniker, ...args) {
     console.assert(bool, "%s is %s", moniker, genus);
 
     Elapida.scorecard(bool);
-
-    console.count('test-completed');
   } catch (anomaly) {
     Elapida.diagnose(moniker, anomaly);
+  } finally {
+    Elapida.tailgate(moniker);
   }
-}
+
+  return;
+};
 
 
-console.time('timeline');
-
-console.timeLog('timeline');
+console.time('timeline'); // start timer
 
 Elapida.examine_returned(null, 'dumpster', undefined);
 
-console.timeLog('timeline');
-
 Elapida.examine_returned(null, 'dumpster', 'arcane');
-
-console.timeLog('timeline');
 
 Elapida.examine_datatype('object', 'zosimos');
 
-console.timeLog('timeline');
-
 Elapida.examine_datatype('object', 'quintet');
-
-console.timeLog('timeline');
 
 Elapida.examine_datatype('object', 'triplet');
 
-console.timeLog('timeline');
-
 Elapida.examine_datatype('object', 'pitches');
-
-console.timeLog('timeline');
 
 Elapida.examine_datatype('object', 'arcane');
 
-console.timeLog('timeline');
-
 Elapida.examine_datatype('object', 'charms');
-
-console.timeLog('timeline');
 
 Elapida.examine_datatype('object', 'glyphs');
 
-console.timeLog('timeline');
-
 Elapida.examine_datatype('object', 'regexps');
-
-console.timeLog('timeline');
 
 Elapida.examine_datatype('object', 'keyhole');
 
-console.timeLog('timeline');
-
 Elapida.examine_datatype('object', 'signatures');
-
-console.timeLog('timeline');
 
 Elapida.examine_datatype('number', 'volume');
 
-console.timeLog('timeline');
-
 Elapida.examine_datatype('string', 'serialStamp');
-
-console.timeLog('timeline');
 
 Elapida.examine_typified('string', 'crucible',
   Panopolis.zosimos['n0'], Panopolis.quintet['cn']);
 
-console.timeLog('timeline');
-
 Elapida.examine_returned(null, 'distillate', undefined);
 
-console.timeLog('timeline');
-
 Elapida.examine_returned(null, 'distillate', 'charms');
-
-console.timeLog('timeline');
 
 Elapida.examine_typified('string', 'pegbox',
   Panopolis.zosimos['n0'], Panopolis.quintet['cn']);
 
-console.timeLog('timeline');
-
 Elapida.examine_returned(null, 'fingerboard', 'quintet',
   Panopolis.zosimos['n0']);
 
-console.timeLog('timeline');
-
 Elapida.examine_returned(null, 'panther', 'j56');
-
-console.timeLog('timeline');
 
 Elapida.examine_returned(null, 'vulture', undefined, undefined);
 
-console.timeLog('timeline');
-
 Elapida.examine_returned(null, 'vulture', 'yq', 'charms');
-
-console.timeLog('timeline');
 
 Elapida.examine_returned(null, 'selections', undefined);
 
-console.timeLog('timeline');
-
 Elapida.examine_returned(null, 'retriever', '-zh', ['k1', 'j0k9', 'k15']);
-
-console.timeLog('timeline');
 
 Elapida.examine_typified('object', 'sentinel',
   Array(Panopolis.volume).fill('k2j56y7h'));
 
-console.timeLog('timeline');
-
 Elapida.examine_returned(null, 'tutorial', undefined);
-
-console.timeLog('timeline');
 
 Elapida.examine_returned(null, 'entryway', []);
 
-console.timeLog('timeline');
-
 Elapida.examine_returned(null, 'entryway', [null, null, 'n0']);
-
-console.timeLog('timeline');
 
 console.log(Elapida.format,
   Elapida.errors, Elapida.failed, Elapida.passed, Elapida.tested
 );
+
+process.stdout.write('\t\tfinal-');
+console.timeEnd('timeline'); // stop timer
 
 
